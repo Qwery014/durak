50829dd6d9345b20ebb4a6d7e56476299f57de4d
Qwery <107665872+Qwery014@users.noreply.github.com>
2023-12-31 21:03:11 +0600

    playPage in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.scss';
 import MainRoutes from './MainRoutes';
+import PlayTablePage from './pages/PlayTablePage';
 
 function App() {
   return (
     <>
       <MainRoutes />
+      <PlayTablePage />
     </>
   );
 }
